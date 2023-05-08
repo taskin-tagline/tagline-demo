e55f305cbfb1b3eb9f2554887c5f20d0df4578b3
taskin-tagline <taskinbanuh.tagline@gmail.com>
2023-05-08 18:29:28 +0530

    First commit

---
+++ src/Reusable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Reusable = () => {
+  return <div>Reusable</div>;
+};
+
+export default Reusable;
---
+++ src/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>This is Git Intro demo</h2>
+    </div>
+  );
+};
+
+export default Settings;
